74b0ca75e21976941423a078dd2fc18009ee592b
Massatriof16 <massatrioo09@gmail.com>
2025-03-01 10:20:12 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,8 @@ AB_OTA_PARTITIONS += \
     dtbo \
     product
 
-
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+$(TARGET_OUT_SHARED_LIBRARIES)/libopenaes.so.toc\
 
 #vendor_boot configuration
 TARGET_NO_RECOVERY := true
