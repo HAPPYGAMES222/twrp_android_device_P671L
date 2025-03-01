89edef175c1bb5f6e74d12cf52e0b6603a67fbcd
Massatriof16 <massatrioo09@gmail.com>
2025-03-02 05:56:16 +0700

    Update twrp_P671L.mk

--- twrp_P671L.mk
+++ twrp_P671L.mk
@@ -4,11 +4,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
+DEVICE_PATH := device/itel/P671L
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Inherit some common Omni stuff.
