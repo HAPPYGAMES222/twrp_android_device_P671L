bf2d5baf5d2ba74b230f6501afe4f6e384f5b613
Massatriof16 <massatrioo09@gmail.com>
2025-03-04 23:54:32 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service \
     libhealthd.$(PRODUCT_PLATFORM)
 #TW_LOAD_VENDOR_MODULES := "goodix_fp.ko  modules.load modules.load.recovery msm_drm.ko sprd_audcp_boot.ko  fpsensor_fp.ko sprd_sensor.ko aw32257_charger.ko charger-manager.ko tran_charger.ko musb_hdrc.ko musb_sprd.ko"
-TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/lib/modules)\")
+TW_LOAD_VENDOR_MODULES := modules.load.recovery adaptive-ts.ko chipone_icnl99xx.ko omnivision_td4160.ko
  
 # Boot control HAL
 PRODUCT_PACKAGES += \
