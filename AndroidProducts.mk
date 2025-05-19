8487847d18afedd78d002ebc46fc9bcaf0d7f2aa
Massatriof16 <massatrioo09@gmail.com>
2025-05-20 06:50:18 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_S685LN.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_S685LN-eng \
-    twrp_S685LN-user \
-    twrp_S685LN-userdebug
+    twrp_S685LN-ap2a-eng \
+    twrp_S685LN-ap2a-user \
+    twrp_S685LN-ap2a-userdebug
