a458538ade34801bc4bf11779d850c7d6ca1c51b
Massatriof16 <massatrioo09@gmail.com>
2025-05-30 22:03:20 +0700

    Update twrp_S685LN.mk

--- twrp_S685LN.mk
+++ twrp_S685LN.mk
@@ -15,7 +15,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from itel-S685LN device
-#$(call inherit-product, device/itel/S685LN/device.mk)
+$(call inherit-product, device/itel/S685LN/device.mk)
 
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
